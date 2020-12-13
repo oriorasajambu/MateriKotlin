c1cc55b266a67058f36b49fbde61ac210ca764f1
oriorasajambu <oriorasajambu@gmail.com>
2020-12-13 21:45:39 +0700

    Pertemuan Pertama

---
+++ src/PertemuanPertama.kt
@@ -0,0 +1,48 @@
+/*
+    1. Comment
+
+    a. 1 Line Comment -> //
+    b. multiple line comment -> /**/
+ */
+
+/*
+    2. Menampilkan text
+
+    a. print("Hello World")
+    b. println("Hello World")
+ */
+
+/*
+    3. Deklarasi Variable
+
+    var -> mutable (data dapat berubah)
+    val -> immutable (data tidak dapat berubah)
+
+    tipe data
+    Int     -> bilangan bulat   cth 0, 1, 2, 3, -3, -123
+    Double  -> bilangan berkoma cth 0.64, 10.3, 13.324
+    Float   -> bilangan berkoma cth 123f
+    String  -> kalimat          cth "Hello World"
+    Char    -> Karakter         cth 'A', 'B'
+    Boolean -> true / false
+
+    cth Deklarasi Variable
+
+    mutable
+    var umur : Int = 10
+    var umur = 10
+    var nama = "Rio Wirawan"
+
+    immutable
+    val phi : Double = 3.14
+    val phi = 3.14
+ */
+
+/*
+
+ */
+
+fun main(){
+
+
+}
